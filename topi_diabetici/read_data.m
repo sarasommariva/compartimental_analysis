ec16de92522a094728e4f8eb74ced8a5e957f8f1
sarasommariva <sommariva@dima.unige.it>
2020-04-29 12:25:26 +0200

    Better defined two groups - more systematic definition of the number of available time-points

--- topi_diabetici/read_data.m
+++ topi_diabetici/read_data.m
@@ -2,75 +2,80 @@ clc
 clear
 close all
 
+%% Step 1. Define general parameters
 folder_data = './data';
 data_path = fullfile(folder_data, ...
     'BRAIN_TOPI_per_analisi_compartimentale.xlsx');
 
-%% Step 1. Read data from file excel
+%% Step 2. Read data from file excel
 
-% 1.1. Time
+% 2.1. Time
 time = xlsread(data_path, 'B5:B31');
 
-% 1.2. First group
-aux_group_1 = xlsread(data_path, 'C5:BN32');
-aux_group_2 = xlsread(data_path, 'BQ5:DT32');
+% 2.2. First group
+aux_group_diab = xlsread(data_path, 'C5:BN32');
+aux_group_control = xlsread(data_path, 'BQ5:DT32');
 
 
-%% Step 2. Organize data into matlab structure
-% 2.1. Size of each group
-n_mice_group_1 = size(aux_group_1, 2)/4;
-n_mice_group_2 = size(aux_group_2, 2)/4;
+%% Step 3. Organize data into matlab structure
+% 3.1. Size of each group
+n_mice_group_diab = size(aux_group_diab, 2)/4;
+n_mice_group_control = size(aux_group_control, 2)/4;
 
-if n_mice_group_1 ~= floor(n_mice_group_1)
+if n_mice_group_diab ~= floor(n_mice_group_diab)
     error('Something went wrong when reading data..')
 end
 
-if n_mice_group_2 ~= floor(n_mice_group_2)
+if n_mice_group_control ~= floor(n_mice_group_control)
     error('Something went wrong when reading data..')
 end
 
 
-% 2.2. Data from each group
+% 3.2. Data from each group
 %       - First group
-mice_group_1(n_mice_group_1) = struct();
-for im = 1:n_mice_group_1
+mice_group_diab(n_mice_group_diab) = struct();
+for im = 1:n_mice_group_diab
     
-    idx_end_if = find(isnan(aux_group_1(:, 4*im-3)), 1) - 1;
-    if isempty(idx_end_if); idx_end_if = size(aux_group_1, 1); end
-    idx_end_wb = find(isnan(aux_group_1(:, 4*im-2)), 1) - 1;
-    if isempty(idx_end_wb); idx_end_wb = size(aux_group_1, 1); end
-    idx_end_ant = find(isnan(aux_group_1(:, 4*im-1)), 1) - 1;
-    if isempty(idx_end_ant); idx_end_ant = size(aux_group_1, 1); end
-    idx_end_post = find(isnan(aux_group_1(:, 4*im)), 1) - 1;
-    if isempty(idx_end_post); idx_end_post = size(aux_group_1, 1); end
-
+    idx_end = zeros(1, 4);
+    for ic = 1:4
+        aux_idx_end = find(isnan(aux_group_diab(:, 4*(im-1)+ic)), 1) - 1;
+        if isempty(aux_idx_end) 
+            idx_end(ic) = size(aux_group_diab, 1); 
+        else
+            idx_end(ic) = aux_idx_end;
+        end
+    end
     
-    mice_group_1(im).name = sprintf('mouse_%d', im);
-    mice_group_1(im).if = aux_group_1(1:idx_end_if, 4*im-3);
-    mice_group_1(im).wb = aux_group_1(1:idx_end_wb, 4*im-2);
-    mice_group_1(im).ant = aux_group_1(1:idx_end_ant, 4*im-1);
-    mice_group_1(im).post = aux_group_1(1:idx_end_post, 4*im);
+    mice_group_diab(im).name = sprintf('mouse_diabetes_%d', im);
+    mice_group_diab(im).if = aux_group_diab(1:idx_end(1), 4*im-3);
+    mice_group_diab(im).wb = aux_group_diab(1:idx_end(2), 4*im-2);
+    mice_group_diab(im).ant = aux_group_diab(1:idx_end(3), 4*im-1);
+    mice_group_diab(im).post = aux_group_diab(1:idx_end(4), 4*im);
     
 end
 
 %       - Second group
-mice_group_2(n_mice_group_2) = struct();
-for im = 1:n_mice_group_2
-    
-    idx_end_if = find(isnan(aux_group_2(:, 4*im-3)), 1) - 1;
-    if isempty(idx_end_if); idx_end_if = size(aux_group_2, 1); end
-    idx_end_wb = find(isnan(aux_group_2(:, 4*im-2)), 1) - 1;
-    if isempty(idx_end_wb); idx_end_wb = size(aux_group_2, 1); end
-    idx_end_ant = find(isnan(aux_group_2(:, 4*im-1)), 1) - 1;
-    if isempty(idx_end_ant); idx_end_ant = size(aux_group_2, 1); end
-    idx_end_post = find(isnan(aux_group_2(:, 4*im)), 1) - 1;
-    if isempty(idx_end_post); idx_end_post = size(aux_group_2, 1); end
+mice_group_control(n_mice_group_control) = struct();
+for im = 1:n_mice_group_control
 
-    
-    mice_group_2(im).name = sprintf('mouse_%d', im);
-    mice_group_2(im).if = aux_group_2(1:idx_end_if, 4*im-3);
-    mice_group_2(im).wb = aux_group_2(1:idx_end_wb, 4*im-2);
-    mice_group_2(im).ant = aux_group_2(1:idx_end_ant, 4*im-1);
-    mice_group_2(im).post = aux_group_2(1:idx_end_post, 4*im);
+    idx_end = zeros(1, 4);
+    for ic = 1:4
+        aux_idx_end = find(isnan(aux_group_control(:, 4*(im-1)+ic)), 1) - 1;
+        if isempty(aux_idx_end) 
+            idx_end(ic) = size(aux_group_control, 1); 
+        else
+            idx_end(ic) = aux_idx_end;
+        end
+    end
+
+    mice_group_control(im).name = sprintf('mouse_%d', im);
+    mice_group_control(im).if = aux_group_control(1:idx_end(1), 4*im-3);
+    mice_group_control(im).wb = aux_group_control(1:idx_end(2), 4*im-2);
+    mice_group_control(im).ant = aux_group_control(1:idx_end(3), 4*im-1);
+    mice_group_control(im).post = aux_group_control(1:idx_end(4), 4*im);
     
 end
+
+%% Step 4. Save
+save(fullfile(folder_data, 'diab_vs_control'), ...
+    'mice_group_diab', 'mice_group_control', 'time')
